6c44dc5122a99fe15a8febfe932273096ff233fc
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-04-01 17:06:31 +0700

    Rest-API

---
+++ seeders/20190401100240-add-admin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin',
+      password: '$2a$08$XOgnvmUFCm8421IX6Bmpw.OwpzsurZ36WuZG./plIxLl0BSB4iQOe',
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Users', null, {})
+  }
+};
